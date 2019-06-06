6f5fe16e6e097aa2d1359cf1d4167fb91235b5d0
s-light <git@s-light.eu>
2019-06-06 18:15:57 +0200

    hopefully fix start script

--- start-photobooth.sh
+++ start-photobooth.sh
@@ -2,7 +2,7 @@
 
 DISPLAY=:0.0 \
 XAUTHORITY=/home/pi/.Xauthority \
-/usr/bin/python3
-/home/pi/python_simple_photobooth_webcam/cam.py \
---camera-device="/dev/video_cam_C1" \
---output_filename="/home/pi/python_simple_photobooth_webcam/captured/test_{date_part}.jpg"
+/usr/bin/python3 \
+    /home/pi/python_simple_photobooth_webcam/cam.py \
+    --camera-device="/dev/video_cam_C1" \
+    --output_filename="/home/pi/python_simple_photobooth_webcam/captured/testpi_{date_part}.jpg"
